35c7273cd5bfbde2115848d08239a8c4255147da
ashmeet07 <ashmeetsingh0123@gmail.com>
2025-07-22 23:41:30 +0530

    update

--- vite.config.js
+++ vite.config.js
@@ -28,7 +28,7 @@ import { visualizer } from 'rollup-plugin-visualizer';
 
 export default defineConfig({
   plugins: [react()],
-  base: '/Tech_Quanta_community_website/', // <--- CHANGE THIS LINE!
+  base: '/', // <--- CHANGE THIS LINE!
   envPrefix: [
     'VITE_',
     'GOOGLE_MACRO_API',
